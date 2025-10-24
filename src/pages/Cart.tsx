2ba3eb7258b49e2961b2fd0c620d735d75acdcff
Hồ Quang Nhân <192851455+NhawnHo@users.noreply.github.com>
2025-10-24 23:07:41 +0700

    update stock

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -111,7 +111,7 @@ function Cart() {
                     'http://localhost:3000';
 
                 await axios.put(
-                    `${apiBaseUrl}/api/products/update-variant-stock`,
+                    `${apiBaseUrl}/api/products/update-multiple-stock`,
                     { items: stockUpdates },
                     { withCredentials: true },
                 );
